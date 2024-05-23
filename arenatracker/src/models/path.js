3a019d9bd6ff2bd596e3af24bd1fdd29b784b435
Albin Stegler <albin.stegler@gmail.com>
2024-05-23 22:06:28 +0200

    url changed

--- arenatracker/src/models/path.js
+++ arenatracker/src/models/path.js
@@ -1,7 +1,7 @@
 const url = {
     getPath: () => {
         if (process.env.NODE_ENV === 'production') {
-            return 'http://16.171.53.167:3001';
+            return 'https://16.171.53.167:3001';
         } else {
             return 'http://localhost:3001';
         }
